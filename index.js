38f7e64fae091f78f9fa21f8df7e03ecf1e1ff09
Chris Byerley <cbyerley@develephant.com>
2015-08-26 05:04:52 -0500

    0.0.17

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ module.exports = function(env) {
 	return {
 		init: function() {
 			env.server.get('/', function( req, res ) {
-				// res.setHeader('Location', '/client');
-				// res.send(302);
-				fs.readFile(__dirname + '/public/index.html', {
-					encoding: 'UTF-8'
-				}, function( err, data ) {
-					res.setHeader('Content-Type', 'text/html');
-					res.send(200, data);
-				})
-			});
-	};
+				res.setHeader('Location', '/public/');
+				res.send(302);
+			// 	fs.readFile(__dirname + '/public/index.html', {
+			// 		encoding: 'UTF-8'
+			// 	}, function( err, data ) {
+			// 		res.setHeader('Content-Type', 'text/html');
+			// 		res.send(200, data);
+			// 	});
+			// });
+		});
+	}
 	// return {
 	// 	init: function() {
 	// 		env.server.get('/', function( req, res ) {
