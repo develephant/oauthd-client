a29a6e7c6a3eb09d63f598fb3dccaa7fdb3f7acb
Chris Byerley <cbyerley@develephant.com>
2015-08-27 12:10:38 -0500

    0.3.14

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 module.exports = function( env ) {
 	return {
 		//create server here
-		start: function ()
+		init: function ()
 		{
 			env.server.get('/home', function( req, res) {
 				res.send(200, 'Hello Home');
