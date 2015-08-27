cd494f07b552566e5cc3a63b01d929516037d991
Chris Byerley <cbyerley@develephant.com>
2015-08-27 13:16:19 -0500

    0.3.22

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var fs = require('fs');
+var restify = require('restify');
+
 module.exports = function( env ) {
 	return {
 		//create server here
 		init: function ()
 		{
-			env.server.get('/home', function( req, res) {
+			env.server.get('/home', function( req, res, next ) {
 				fs.readFile(__dirname+'/public/index.html', function( err, data ) {
-					res.setHeader('Content-Type','text/html');
-					res.send(200, data);
+					if ( !err ) {
+						res.setHeader('Content-Type','text/html');
+						res.send(200, data);
+					} else {
+						next();
+					}
 				});
 			});
+
+			env.server.get(/^\/(.*)/, restify.serveStatic({
+				directory: __dirname + '/public',
+	      default: __dirname + '/public/index.html'
+			}));
 		}
 	}; //return
 };
