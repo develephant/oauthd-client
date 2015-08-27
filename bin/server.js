00d2021a9076b28d24d4544d33bb094344566581
Chris Byerley <cbyerley@develephant.com>
2015-08-27 08:15:52 -0500

    0.2.10

---
+++ bin/server.js
@@ -0,0 +1,35 @@
+var ecstatic, fs, restify;
+
+restify = require('restify');
+ecstatic = require('ecstatic');
+fs = require('fs');
+
+module.exports = function(env) {
+  var exp;
+  exp = {};
+  exp.setup = function(callback) {
+    env.server.get(/^(\/.*)/, function(req, res, next) {
+      if (req.params[0] === '/') {
+            res.setHeader('Location', '/dashboard');
+            res.send(302);
+      }
+      return fs.stat(__dirname + '/public' + req.params[0], function(err, stat) {
+        if ( stat.isFile() === false ) {
+          next();
+        } else {
+          return fs.readFile(__dirname + '/public/index.html', {
+            encoding: 'UTF-8'
+          }, function(err, data) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(200, data);
+          });
+        }
+      });
+    }, restify.serveStatic({
+      directory: __dirname + '/public',
+      "default": __dirname + '/public/index.html'
+    }));
+    return callback();
+  };
+  return exp;
+};
--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-var fs = require('fs');
 module.exports = function(env) {
-	return {
-		init: function() {
-			env.server.get('/dashboard', function( req, res ) {
-				fs.readFile(__dirname+'/public/index.html',{encoding:'UTF-8'},function(err,data) {
-
-					res.setHeader('Content-Type','text/html');
-					res.send(200, data);
-
-				}
-			})
-		}
-	}
+	var plugin = require('./bin/server.js')(env);
+	return plugin;
 }
