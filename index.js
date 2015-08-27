f8d7b69bc415200ff0cc6bc739b1c70b6133b167
Chris Byerley <cbyerley@develephant.com>
2015-08-26 23:28:16 -0500

    0.2.4

--- index.js
+++ index.js
@@ -2,32 +2,14 @@ var fs = require('fs');
 module.exports = function(env) {
 	return {
 		init: function() {
-			env.server.get('/dashboard', function( req, res, next ) {
+			env.server.get('/dashboard', function( req, res ) {
 				fs.readFile(__dirname+'/public/index.html',{encoding:'UTC-8'},function(err,data) {
 					if ( !err ) {
 						res.setHeader('Content-Type','text/html');
 						res.send(200, data);
 					}
 				}
-			}),
-			env.server.get(/^(\/.*)/, function( req, res, next ) {
-				if (req.params[0] === '/') {
-		      res.setHeader('Location', '/dashboard');
-		      res.send(302);
-				} else { //check for static file
-					fs.stat(__dirname+'/public'+req.params[0], function(err,stats) {
-						if ( !err ) {
-							if (stats.isFile() === true ) {
-								fs.readFile(__dirname+'/public'+req.params[0],{encoding:'UTC-8'},function(err,data) {
-									if ( !err ) {
-										res.send(200, data);
-									}
-								});
-							}
-						}
-					});
-				}
-			});
+			})
 		}
 	}
 }
