91b07d9b0d2db59dce8cacd3fc6e4ac7ea104b7a
Chris Byerley <cbyerley@develephant.com>
2015-08-27 11:51:27 -0500

    0.3.10

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 module.exports = function(env) {
 	//create server here
-	env.server.get('/home', function( err, data ) {
+	env.server.get('/home', function( req, res ) {
 		env.debug( err );
-		fs.readFile(__dirname+'/public/index.html', function( req, res ) {
+		fs.readFile(__dirname+'/public/index.html', function( err, data ) {
 			res.setHeader('Content-Type', 'text/html');
 			res.send(200, data);
 		});
