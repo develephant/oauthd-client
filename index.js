f5454ae19d082e61449739fb9423a81ca5ddf1c0
Chris Byerley <cbyerley@develephant.com>
2015-08-26 22:14:21 -0500

    0.1.6

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ module.exports = function(env) {
 			env.server.get('/dashboard', function( req, res ) {
 				fs.readFile(__dirname+'/public/index.html',{encoding:'UTF-8'},function(err,data) {
 					if ( !err ) {
+						res.setHeader('Content-Type','text/html');
 						res.send(200, data);
 					}
 				});
