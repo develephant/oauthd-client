8440955db703ff60e70427ba52665b5b005d5477
Chris Byerley <cbyerley@develephant.com>
2015-08-26 20:00:41 -0500

    0.0.22

--- index.js
+++ index.js
@@ -3,29 +3,21 @@ module.exports = function(env) {
 	return {
 		init: function() {
 			env.server.get(/^(\/.*)/, function( req, res, next ) {
-				//redirect to dashboard
-				// if (req.params[0] === '/') {
-				// 	res.setHeader('Location', '/dashboard');
-				// 	res.send(302);
-				// }
 
 				//Check for file
-				fs.stat(__dirname + '/public' + req.params[0],
-				function( err, stats ) {
+				fs.stat(__dirname + '/public' + req.params[0], function( err, stats ) {
 					if ( err || ( stats.isFile() === false ) ) {
 						next();
-					} else { //no error
-						fs.readFile((__dirname+'/public/index.html',
-						{ encoding: 'UTF-8' },
-						function( err, data) {
-							if ( !err ) {
+					} else { //no error - serve up index
+						fs.readFile(__dirname+'/public/index.html', {encoding: 'UTC-8'}, function(err, data) {
+							if (!err) {
 								res.setHeader('Content-Type', 'text/html');
 								res.send(200, data);
 							}
-						}));
+						});
 					}
 				});
-			});
+			}
 		}
-	};
+	}
 };
