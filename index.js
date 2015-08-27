a48f0ec122005f38e0eb3980799a9f16c1f73da9
Chris Byerley <cbyerley@develephant.com>
2015-08-27 13:54:46 -0500

    0.3.26

--- index.js
+++ index.js
@@ -8,38 +8,9 @@ module.exports = function( env ) {
 		//create server here
 		init: function ()
 		{
-			env.server.get(/^(\/.*)/, function( req, res, next ) {
-				var params;
-				if (req.params[0].length > 0 ) {
-					params = req.params[0];
-				}
-
-				if ( params === '/') {
-					res.setHeader('Location','/client');
-					res.send(302);
-				}
-
-				fs.stat(__dirname+'/public'+params, function( err, stat ) {
-					if ( !err ) {
-						if ( stat.isFile() === true ) {
-							fs.readFile(__dirname+'/public/'+params, {encoding: 'UTF-8'}, function( err, data) {
-								if ( !err ) {
-									res.send(200, data);
-								} else {
-									next();
-								}
-							})
-						} else {
-							next();
-						}
-					} else {
-						next();
-					}
-				})
-			});
-		}, restify.serveStatic({
-			directory: __dirname + '/public',
-			default: __dirname + '/public/index.html'
-		});
-	}; //return
+			env.server.get(/^(\/.*)/, function( req, res ) {
+				res.send(200, req.params[0]);
+			}
+		}
+	}
 };
