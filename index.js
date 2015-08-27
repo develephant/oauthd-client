21af4c6924a682fd4546f48d132a0a2ce4a7075a
Chris Byerley <cbyerley@develephant.com>
2015-08-27 14:03:22 -0500

    0.3.28

--- index.js
+++ index.js
@@ -6,14 +6,11 @@ restify = require('restify');
 module.exports = function( env ) {
 	return {
 		//create server here
-		init: function ()
-		{
-			env.server.get('/params', function( req, res ) {
-				res.send(200, req.params[0]);
-			}
+		env.server.get('/params', function( req, res ) {
+			res.send(200, req.params[0]);
+		}
 			// env.server.get(/^(\/.*)/, function( req, res ) {
 			// 	res.send(200, req.params[0]);
 			// }
-		}
 	}
 };
