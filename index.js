444103ce1a61589e80eccd2803a21d3d0d75f3f2
Chris Byerley <cbyerley@develephant.com>
2015-08-27 13:57:54 -0500

    0.3.27

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ module.exports = function( env ) {
 		//create server here
 		init: function ()
 		{
-			env.server.get(/^(\/.*)/, function( req, res ) {
+			env.server.get('/params', function( req, res ) {
 				res.send(200, req.params[0]);
 			}
+			// env.server.get(/^(\/.*)/, function( req, res ) {
+			// 	res.send(200, req.params[0]);
+			// }
 		}
 	}
 };
