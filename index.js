a99fc8303d8c35b3c0c0308fc355cc8439174d19
Chris Byerley <cbyerley@develephant.com>
2015-08-27 11:54:22 -0500

    0.3.11

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var fs = require('fs');
 module.exports = function(env) {
 	//create server here
 	env.server.get('/home', function( req, res ) {
-		env.debug( err );
 		fs.readFile(__dirname+'/public/index.html', function( err, data ) {
-			res.setHeader('Content-Type', 'text/html');
+			env.debug( err );
+			//res.setHeader('Content-Type', 'text/html');
 			res.send(200, data);
 		});
 	});
