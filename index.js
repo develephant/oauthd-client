64b23998b838c8e2534339c758815473c7c2e2b6
Chris Byerley <cbyerley@develephant.com>
2015-08-27 11:56:43 -0500

    0.3.12

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ module.exports = function(env) {
 	//create server here
 	env.server.get('/home', function( req, res ) {
 		fs.readFile(__dirname+'/public/index.html', function( err, data ) {
-			env.debug( err );
+			//env.debug( err );
 			//res.setHeader('Content-Type', 'text/html');
-			res.send(200, data);
+			res.send(200, 'Yolo');
 		});
 	});
 };
